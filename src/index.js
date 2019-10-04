404354ccfd74ddc5286076882500f6118518104d
romasr <shaman.roman@tut.by>
2019-10-05 01:22:14 +0300

    feat: add new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length %2 ) {
+        return false;
+      }
+    
+      const {open, close} = bracketsConfig.reduce((acc, curr) => {
+          acc.open.push(curr[0]);
+          acc.close.push(curr[1]);
+          return acc;
+      }, {open:[], close:[]});
+    
+      const stack =[];
+    
+      [...str].forEach((el, idx)=>{
+        let index = close.findIndex(s=>s===el);
+        let openSymbol = open[index];
+        let flag = true;
+          if (close.includes(el)) {
+            if (stack[stack.length-1]===openSymbol){ 
+              stack.pop();
+              flag = false;
+          }
+        }
+        if (open.includes(el) && flag) {
+          stack.push(el);
+        }
+      });
+        
+     if (stack.length !== 0){
+        return false;
+      }
+      return true;
 }
